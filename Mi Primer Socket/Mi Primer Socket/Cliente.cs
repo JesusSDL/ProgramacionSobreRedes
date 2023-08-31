4f281b5639e7f6b38732f17589ff87515daee56e
Jesús Santiago Di Lorenzo <117871940+JesusSDL@users.noreply.github.com>
2023-08-31 13:52:25 -0300

    Add files via upload

---
+++ Mi Primer Socket/Mi Primer Socket/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+public class Cliente	
+{
+    static void Main(string[] args)
+    {
+        Socket miSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint miDir = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+        try
+        {
+         miSocket.Connect(miDir);
+            Console.WriteLine("conectado");
+            String texto = "hola";
+            byte[] aEnviar = Encoding.Default.GetBytes(texto);
+            miSocket.Send(aEnviar);
+            Console.WriteLine(texto);
+            byte[] byterecib= new byte[255];
+            int numrec=miSocket.Receive(byterecib,0,byterecib.Count(),0);
+            Array.Resize(ref byterecib,numrec);
+            String txtrec = Encoding.Default.GetString(byterecib);
+            Console.WriteLine(txtrec);
+            String nombre= Console.ReadLine();
+            byte[] otrotxt= Encoding.Default.GetBytes(nombre);
+            miSocket.Send(otrotxt, 0, otrotxt.Count(), 0);
+            Console.WriteLine("envio : " + nombre);
+            byte[] byterecibido = new byte[255];
+            int numero = miSocket.Receive(byterecibido,0,byterecibido.Count(),0);
+            Array.Resize(ref byterecibido,numrec);
+            txtrec = Encoding.Default.GetString(byterecibido);
+            Console.WriteLine(txtrec);
+            miSocket.Close();
+            
+        }
+        catch (Exception error)
+        {
+
+            Console.WriteLine(error.ToString());
+
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Servidor/Servidor/Servidor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Servidor
+{
+    static void Main(string[] args) { 
+    Socket miSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    IPEndPoint miDir = new IPEndPoint(IPAddress.Any, 1234);
+
+     try{
+         miSocket.Bind(miDir);
+            miSocket.Listen(1);
+            Console.WriteLine("Escuchando...");
+            Socket escuchar = miSocket.Accept();
+            Console.WriteLine("Conecta2");
+            Byte[] byRecv = new byte[255];
+            int enteroRecibido = escuchar.Receive(byRecv, 0, byRecv.Count(), 0);
+            Array.Resize(ref byRecv, enteroRecibido);
+            String elMensaje = Encoding.Default.GetString(byRecv);
+            Console.WriteLine(elMensaje);
+            String texto = "nombre?";
+            Byte[] txtAEnviar = Encoding.Default.GetBytes(texto);
+            miSocket.Send(txtAEnviar, 0, txtAEnviar.Count(), 0);
+            Console.WriteLine(texto);
+            Byte[] byteRecv = new byte[255];
+            enteroRecibido = escuchar.Receive(byteRecv, 0, byteRecv.Count(), 0);
+            Array.Resize(ref  byteRecv, enteroRecibido);
+            elMensaje = Encoding.Default.GetString((byte[])byteRecv);
+            Console.WriteLine(elMensaje);
+            String nuevoMensaje = "Hola " + elMensaje;
+            Byte[] byRepartir = Encoding.Default.GetBytes(nuevoMensaje);
+            miSocket.Send(byRepartir, 0, byRepartir.Count(), 0);
+            Console.WriteLine(nuevoMensaje);
+            miSocket.Close();
+        }
+        catch(Exception error){
+
+        Console.WriteLine(error.ToString());
+
+        }
+        Console.ReadKey();
+    }
+}
